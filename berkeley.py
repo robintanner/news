4edf307d48c1672fc2820bc71e0cf87bde717b6e
robintanner <robin.tanner@inteledyne.com>
2015-07-15 19:30:35 -0400

    Committing kafka python package files and changes to news streaming python code

--- berkeley.py
+++ berkeley.py
@@ -9,9 +9,13 @@ import datetime
 import logging
 import argparse
 import sys
+
+
 #api = articleAPI('11570be9bf6f23e37211b7fd38939195:18:72364286')
 #articles = api.search(q = 'Obama')
-def getArticles(sdate, edate, api_key):
+
+
+def getArticles(sdate, edate, url, api_key):
      for date in daterange(sdate, edate):
           art_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + date.strftime("%Y%m%d") + "&end_date=" + date.strftime("%Y%m%d") + "&api-key=" + api_key
           articles = requests.get(art_url).json()
@@ -21,7 +25,10 @@ def getArticles(sdate, edate, api_key):
           articles = jsonview.json()['response']['docs']
           print(articles)
           for article in articles:
-               print("published: ", article['pub_date'], article['snippet'])
+              if 'keyword' in article:
+                  print(article['keyword'][0])
+
+              print("published: ", article['pub_date'], article['snippet'])
 
           #print (json.dumps(jsonview.json()))
 
@@ -36,19 +43,48 @@ def daterange( start_date, end_date ):
             yield start_date - datetime.timedelta( n )
 
 def Main():
-    #searchtype = input("Please enter a search type:")
-    nytfin_url = "http://api.nytimes.com/svc/elections/us/v3/finances/"
-    art_key = "836afc9bbe86ee8c2a1236478f332bc4:11:72364286"
-    nytfin_key = "&api-key=11570be9bf6f23e37211b7fd38939195:18:72364286"
-    nytfin_cycle = "2008"
-    nytfin_lname_query = "obama"
-    nytfin_candidates_search_partial = "/candidates/search"
+
+
+
+
+    #global parameters
+    parser = argparse.ArgumentParser(description="A Python tool for grabbing data from the NY Times API")
+    parser.add_argument('-j', '--json', required=True, help="path to the folder where you want JSON files stored")
+    #args = parser.parse_args()
+
+    #json_file_path = args.json
+
+    #log_file = "".join([json_file_path, "getTimesArticles_testing.log"])
+    #logging.basicConfig(filename=log_file, level=logging.INFO)
+    #logging.info("Getting started.")
+
 
     response_format = ".json"
     query = "?query="
     callback = "&callback=JSONP"
     start = datetime.date(year = 2013, month = 1, day = 1)
     end = datetime.date(year = 2013, month = 1, day = 1)
-    getArticles(start, end, art_key)
+
+
+    #input parameters
+    #searchtype = input("Please enter a search type:")
+
+    #user conditions
+    #if searchtype == "campaign finance":
+    #    url = "http://api.nytimes.com/svc/elections/us/v3/finances/"
+    #    key = "11570be9bf6f23e37211b7fd38939195:18:72364286"
+    #    nytfin_cycle = "2008"
+    #    nytfin_lname_query = "obama"
+    #    nytfin_candidates_search_partial = "/candidates/search"
+    #elif searchtype == "articles":
+    #    key = "836afc9bbe86ee8c2a1236478f332bc4:11:72364286"
+    #    url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date="
+
+    key = "836afc9bbe86ee8c2a1236478f332bc4:11:72364286"
+    url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date="
+
+    getArticles(start, end, url, key)
+
+
 
 Main()
